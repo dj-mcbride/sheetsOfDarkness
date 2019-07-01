4194441a4839cb2c9b732395b8a07375830746f6
D.J. McBride <dj.mcbride@itential.com>
2019-07-01 15:53:51 -0400

    adding basic auth

---
+++ index.js
@@ -0,0 +1,52 @@
+require('dotenv').config();
+
+// Define our constants
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Set the apps that we will use
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(express.urlencoded({ extended: true }));
+app.use('/static', express.static('public'));
+
+// auth middleware code
+app.use(require('express-session')({
+    secret: process.env.APP_SECRET,
+    resave: true,
+    saveUninitialized: false
+  }));
+
+  const { ExpressOIDC } = require('@okta/oidc-middleware');
+  const oidc = new ExpressOIDC({
+    issuer: `${process.env.OKTA_ORG_URL}/oauth2/default`,
+    client_id: process.env.OKTA_CLIENT_ID,
+    client_secret: process.env.OKTA_CLIENT_SECRET,
+    redirect_uri: `${process.env.HOST_URL}/authorization-code/callback`,
+    scope: 'openid profile'
+  });
+
+  app.use(oidc.router);
+// Registration page
+app.use('/register', require('./routes/register'));
+
+// @TODO add logout route
+app.get('/logout', (req, res) => {
+    if (req.userContext) {
+      const idToken = req.userContext.tokens.id_token;
+      const to = encodeURI(process.env.HOST_URL);
+      const params = `id_token_hint=${idToken}&post_logout_redirect_uri=${to}`;
+      req.logout();
+      res.redirect(`${process.env.OKTA_ORG_URL}/oauth2/default/v1/logout?${params}`);
+    } else {
+      res.redirect('/');
+    }
+  });
+
+// Build the index
+app.use('/', require('./routes/index'));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`App listening on port ${port}`));
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Sheets of Darkness' });
+router.get('/', (req, res) => {
+  const { userContext } = req;
+  res.render('index', { userContext });
 });
 
 module.exports = router;
